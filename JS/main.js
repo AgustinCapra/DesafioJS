bd04ab849718441ea7f5fd45920bcd86463bd54b
Agustin Capra <agustincapra2@gmail.com>
2022-05-24 23:49:36 +0200

    Desafio simplificar

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,42 @@
+let cantidadInvitados = 0;
+let entrada = document.getElementById('entrada');
+let btn = document.getElementById('btn');
+let actualizado = document.getElementById("ultimo");
+let listaInvitados = document.getElementById("lista")
+let cantidadTotal = document.getElementById('cantidad');
+
+btn.innerHTML = 'Agregar invitado';
+btn.addEventListener('click', function() {
+    cantidadInvitados++;
+    let invitado = entrada.value;
+    if (invitado != "") {
+        actualizado.innerHTML = 'El último invitado/a agregado fue: ' + invitado;
+        let invitados = document.createElement('li');
+        let txt = document.createTextNode(invitado);
+        invitados.appendChild(txt);
+        listaInvitados.appendChild(invitados);
+
+        /* if (cantidadInvitados > 1) {
+            cantidadTotal.innerHTML = `Invitados totales: ${cantidadInvitados} personas.`
+        }else{
+            cantidadTotal.innerHTML = `Invitados totales: ${cantidadInvitados} persona.`
+        } */
+        cantidadInvitados > 1 ? cantidadTotal.innerHTML = `Invitados totales: ${cantidadInvitados} personas.` : cantidadTotal.innerHTML = `Invitados totales: ${cantidadInvitados} persona.`
+    }
+})
+
+document.addEventListener("click", function (e) {
+    if (e.target.localName == 'li') {
+        e.target.classList.add('asistio')
+    }
+})
+
+
+
+
+
 //Desafio complementario Nº 2 ARRAYS
-class Jugador {
+/* class Jugador {
     constructor (nombre, numero, edad, lesionado) {
         this.nombre = nombre;
         this.numero = numero;
@@ -20,4 +57,4 @@ let jugador5 = new Jugador ('Agustin', '1', '68', false);
 jugador2.lesion();
 
 const equipoTitular = [jugador1, jugador2, jugador3, jugador4, jugador5];
-console.log(equipoTitular);
+console.log(equipoTitular); */
